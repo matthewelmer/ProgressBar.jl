414b584545de303793b67192b41c70196370d4a8
Matthew Elmer <>
2024-02-27 11:22:54 -0600

    test progress bar

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,14 +2,12 @@
 
 using ProgressBar
 
-# Progress bar
-println(join([Char(0x2501) for _ in 1:80]))
+print(simple_bar(0))
+sleep(1)
 
-# Progress bar with single-digit percentage
-println(join(vcat([Char(0x2501) for _ in 1:40], "9%", [Char(0x2501) for _ in 43:80])))
-
-# Progress bar with double-digit percentage
-println(join(vcat([Char(0x2501) for _ in 1:39], "69%", [Char(0x2501) for _ in 43:80])))
-
-# Progress bar with triple-digit (100%, done) percentage
-println(join(vcat([Char(0x2501) for _ in 1:38], "100%", [Char(0x2501) for _ in 43:80])))
+for i in 1:100
+    sleep(0.05)
+    print(join([Char(0x8) for _ in 1:80]))
+    print(simple_bar(i, percent=true))
+end
+println()
