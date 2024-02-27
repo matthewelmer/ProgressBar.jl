e2a6d88bb9893d0f176c7532a9e6ae8897dd5948
Matthew Elmer <>
2024-02-27 10:47:41 -0600

    Prototype some BARS

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,15 @@
 #!/usr/bin/env julia
 
 using ProgressBar
-using Test
 
-@testset "basic bar" begin
-    a = 69
-    @test a == 69
-end
+# Progress bar
+println(join([Char(0x2501) for _ in 1:80]))
+
+# Progress bar with single-digit percentage
+println(join(vcat([Char(0x2501) for _ in 1:40], "9%", [Char(0x2501) for _ in 43:80])))
+
+# Progress bar with double-digit percentage
+println(join(vcat([Char(0x2501) for _ in 1:39], "69%", [Char(0x2501) for _ in 43:80])))
+
+# Progress bar with triple-digit (100%, done) percentage
+println(join(vcat([Char(0x2501) for _ in 1:38], "100%", [Char(0x2501) for _ in 43:80])))
