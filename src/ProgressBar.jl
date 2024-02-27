c44a48982ec1ab49c5ed70c3d8b59f2d5e482788
Matthew Elmer <>
2024-02-27 11:41:57 -0600

    make progress bar reflect percentage

--- src/ProgressBar.jl
+++ src/ProgressBar.jl
@@ -6,19 +6,27 @@ export simple_bar
 
 function simple_bar(completion::Real; percent::Bool=false)
     percent_completion::Int = 0
+    pereighty_completion::Int = 0
     if percent
         percent_completion = round(min(completion, 100))
+        pereighty_completion = round(min(completion * 80 / 100, 80))
     else
         percent_completion = round(min(100 * completion, 100))
+        pereighty_completion = round(min(80 * completion, 80))
     end
 
-    num_leading_characters = 41 - ndigits(percent_completion)
-    num_trailing_characters = 38
+    left_pad = 41 - ndigits(percent_completion)
+    right_pad = 38
+
+    left_filled = min(pereighty_completion, left_pad)
+    right_filled = min(max(pereighty_completion - 42, 0), right_pad)
 
     return join(vcat(
-        [Char(0x2501) for _ in 1:num_leading_characters],
+        [Char(0x2501) for _ in 1:left_filled],
+        [" " for _ in (left_filled + 1):left_pad],
         string(percent_completion) * "%",
-        [Char(0x2501) for _ in 1:num_trailing_characters]
+        [Char(0x2501) for _ in 1:right_filled],
+        [" " for _ in (right_filled + 1):right_pad]
     ))
 end
 
