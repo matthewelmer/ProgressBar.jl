c6086498155cae6c28923912634a584698893afa
Matthew Elmer <>
2024-02-27 11:14:24 -0600

    add simple_bar function

--- src/ProgressBar.jl
+++ src/ProgressBar.jl
@@ -1,5 +1,25 @@
 module ProgressBar
+export simple_bar
 
-greet() = print("Hello World!")
+# One way to make a unicode character: Char(0x2501)
+# Another way: "\u2501"
+
+function simple_bar(completion::Real; percent::Bool=false)
+    percent_completion::Int = 0
+    if percent
+        percent_completion = round(min(completion, 100))
+    else
+        percent_completion = round(min(100 * completion, 100))
+    end
+
+    num_leading_characters = 41 - ndigits(percent_completion)
+    num_trailing_characters = 38
+
+    return join(vcat(
+        [Char(0x2501) for _ in 1:num_leading_characters],
+        string(percent_completion) * "%",
+        [Char(0x2501) for _ in 1:num_trailing_characters]
+    ))
+end
 
 end
