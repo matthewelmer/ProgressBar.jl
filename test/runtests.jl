d07b8ced5321c3ce9fd9cfa23ab8e690fceb5dbc
Matthew Elmer <>
2024-02-27 14:20:22 -0600

    quicken up tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ using ProgressBar
 println("Plain bar:")
 print(plain_bar(0))
 for i in 1:100
-    sleep(0.05)
+    sleep(0.025)
     print(join([Char(0x8) for _ in 1:80]))
     print(plain_bar(i, percent=true))
 end
@@ -14,7 +14,7 @@ println()
 println("Inferno bar:")
 print(colorful_bar(0, rangescale=(-0.4, 1.0)))
 for i in 1:100
-    sleep(0.05)
+    sleep(0.025)
     print(join([Char(0x8) for _ in 1:80]))
     print(colorful_bar(i, percent=true, rangescale=(-0.4, 1.0)))
 end
@@ -23,7 +23,7 @@ println()
 println("Inferno bar (changes to green when done):")
 print(colorful_bar(0, rangescale=(-0.4, 1.0)))
 for i in 1:100
-    sleep(0.05)
+    sleep(0.025)
     print(join([Char(0x8) for _ in 1:80]))
     print(colorful_bar(i, percent=true, rangescale=(-0.4, 1.0)))
 end
@@ -33,7 +33,7 @@ println(GREEN_COMPLETED_BAR)
 println("Winter bar:")
 print(colorful_bar(0, cscheme=colorschemes[:winter]))
 for i in 1:100
-    sleep(0.05)
+    sleep(0.025)
     print(join([Char(0x8) for _ in 1:80]))
     print(colorful_bar(i, percent=true, cscheme=colorschemes[:winter]))
 end
